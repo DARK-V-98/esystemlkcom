7bee78b9b6f9df9aeecc0770f33f88d1c6d4f99e
esystemlk <esystemlk@gmail.com>
2026-04-09 23:00:59 +0530

    32

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,6 +69,9 @@ export const metadata: Metadata = {
     apple: "/apple-touch-icon.png",
   },
   manifest: "/manifest.json",
+  other: {
+    "google-adsense-account": "ca-pub-1128178534139221",
+  },
 };
 
 
